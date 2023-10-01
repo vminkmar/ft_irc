80a5bdd84c5388de0472461fd45bf399d7b937db
jweeeezy <willertjakob@gmail.com>
2023-10-01 22:39:44 +0200

    test.cpp: Included missing header

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 
 #include <vector>              // needed for std::vector
 #include <cstdlib>             // needed for MACROS
+#include <iostream>            // needed for std::cout, std::endl
 
 #include "include/Channel.hpp" // needed for Channel class
 #include "include/User.hpp"    // needed for User class
