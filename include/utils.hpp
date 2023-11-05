b548e9926906846a5d18777470415e94d457df11
jweeeezy <willertjakob@gmail.com>
2023-11-05 23:55:12 +0100

    adds missing files

---
+++ include/utils.hpp
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------- //
+
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+#include "typedefs.hpp"
+
+    /* converts an integer to a const std::string */
+    t_str_c   itostr(int i);
+
+    /* summarizes all elements of a vector (std::string)
+     * into one string, divided by a space */
+    t_str_c   sumParameters(t_vec_str_cit start, t_vec_str_cit end);
+
+    /* splits a std::string divided by a delimiter
+     * and stores it in a vector (std::string) */
+    t_vec_str split(t_str_c& parameter, char delimiter);
+
+#endif // UTILS_HPP
+
+// -------------------------------------------------------------------------- //
---
+++ src/utils.cpp
@@ -0,0 +1,46 @@
+// -------------------------------------------------------------------------- //
+
+#include "../include/utils.hpp" // needed for declaration
+
+#include <sstream> // needed for std::stringstream
+
+t_str_c itostr(int i)
+{
+    std::stringstream ss;
+    ss << i;
+    return ss.str();
+}
+
+t_str_c sumParameters(t_vec_str_cit start, t_vec_str_cit end)
+{
+    if (start == end)
+    {
+        throw std::runtime_error("sumParameters: Wrong iterator given!");
+    }
+    std::stringstream ss;
+    t_vec_str_cit last = end - 1;
+    for (t_vec_str_cit it = start; it != end; ++it)
+    {
+        ss << *it;
+        if (it != last)
+        {
+            ss << " ";
+        }
+    }
+    return ss.str();
+}
+
+t_vec_str split(t_str_c &parameter, char delimiter)
+{
+    t_vec_str split;
+    t_str token;
+    std::stringstream ss(parameter);
+
+    while (std::getline(ss, token, delimiter))
+    {
+        split.push_back(token);
+    }
+    return split;
+}
+
+// -------------------------------------------------------------------------- //
